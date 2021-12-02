cce81885065097e0c4182155e9e130c40bc4e645
Piret Saar <piret.saar.003@khk.ee>
2021-12-02 11:41:56 +0200

    Set city to LS and get changed weather data from LS

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+// LS object
+const ls = new LS()
+// init city data
+const initCity = ls.getCity()
 // Weather object
-const weather = new Weather('Tallinn')
+const weather = new Weather(initCity)
 //UI object
 const ui = new UI()
+
 
 // events
 const form = document.querySelector('#change-city')
@@ -11,6 +16,7 @@ form.addEventListener('submit', changeWeather)
 function changeWeather(event){
     const city = document.querySelector('#city-name').value
     weather.changeCity(city)
+    ls.setCity(city)
     getWeather()
     document.querySelector('#city-name').value = ''
     event.preventDefault()
