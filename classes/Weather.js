81a1b0e8306e50c418af03e6e03997172d8ed4b1
Piret Saar <piret.saar.003@khk.ee>
2021-12-02 10:57:34 +0200

    Get weather data from API

---
+++ classes/Weather.js
@@ -0,0 +1,11 @@
+class Weather{
+    constructor(city){
+        this.city = city
+        this.key = '6d57ccdea3baf774719ab0b7ae95c51c'
+    }
+    async getWeather(){
+        const responce = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`)
+        const responceData = await responce.json()
+        return responceData
+    }
+}
